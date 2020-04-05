4f506a9d001adb1bc34650f359819ea396ed4cfd
ivan putra widyatama hukom <ivanpwh@gmail.com>
2020-04-05 16:24:30 +0700

    create micro-project

---
+++ src/api/github.api.js
@@ -0,0 +1,16 @@
+import axios from 'axios'
+
+// get data user repositories in github
+export const getUserRepositories = (user) => axios.get(`users/${user}/repos`)
+  .then(res => res)
+  .catch((err) => {
+    throw err.response
+  })
+
+// get data Read Me in user repository
+export const getMarkdown = (user, repository) => axios.get(`repos/${user}/${repository}/readme`)
+  .then(res => res)
+  .catch((err) => {
+    console.log('err :', err.response);
+    throw err.response
+  })
